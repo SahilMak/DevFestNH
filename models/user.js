c8296a47dbdd775e982e1f0ad63321c1d56c79b2
Sahil Makhijani <svmakhijani94@gmail.com>
2017-11-05 13:19:15 -0500

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var User = new Schema({
+    username: String,
+    password: String,
+    OauthId: String,
+    OauthToken: String,
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    }
+});
+
+User.methods.getName = function () {
+    return (this.firstname + ' ' + this.lastname);
+};
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
---
+++ routes/users.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var User = require('../models/user');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    User.find({}, function (err, user) {
+        if (err) throw err;
+
+        // load user page
+    })
+});
+
+router.post('/register', function (req, res, next) {
+    User.register(new User({username: req.body.username}), req.body.password, function (err, user) {
+        if (err) return res.status(500).json({err: err});
+
+        if (req.body.firstname) user.firstname = req.body.firstname;
+        if (req.body.lastname) user.lastname = req.body.lastname;
+
+        user.save(function (err, user) {
+            passport.authenticate('local')(req, res, function () {
+                return res.status(200).json({status: "Registration successful!"});
+            });
+        });
+    });
+});
+
+router.post('/login', function (req, res, next) {
+    passport.authenticate('local', function (err, user, info) {
+        if (err) return next(err);
+
+        if (!user) return res.status(401).json({err: 'Could not log in user'});
+
+        req.logIn(user, function (err) {
+            //
+        });
+    })(req, res, next);
+});
+
+module.exports = router;
